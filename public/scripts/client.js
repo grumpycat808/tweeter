52c38f2bf377db13f844ac516622b16deae6dcf4
Catherine Li <catherine.l@intiveo.com>
2021-04-10 09:50:29 -0700

    Load tweets via ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,30 +3,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1617842642225
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1617929042225
-    }
-]
+
 const createTweet = (tweet) => {
     
     let $tweet = 
@@ -48,18 +25,43 @@ const createTweet = (tweet) => {
 }
 
 const renderTweets = (tweets) => {
-    // for(tweet in tweets) {
-    //     console.log(tweet);
-    //     $("#tweets-container").append(createTweet(tweet));
-    // }
 
     tweets.forEach(tweet => {
-
         let $newTweet = createTweet(tweet);
-        console.log($newTweet)
         $("#tweets-container").append($newTweet);
     });
 }
-$(document).ready(function(){
-    renderTweets(data);
+$(document).ready(function(data){
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: '/tweets',
+            data: $(this).serialize(),
+            success: () => {
+                console.log("success")
+            },
+            error: () => {
+                console.log("Error")
+            }
+        });
+        
+    });
+
+    const loadTweets = () => {
+        $.ajax({
+            type: "GET",
+            url: '/tweets',
+            data: $(this).serialize(),
+            success: (res) => {
+                renderTweets(res);
+            },
+            error: () => {
+                console.log("Error")
+            }
+        });
+    }
+
+    loadTweets();
 })
