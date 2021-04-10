f40e1df08f6018f3448ae03627d04919ba6e515d
Catherine Li <catherine.l@intiveo.com>
2021-04-09 17:43:32 -0700

    complete createTweets function to turn js object into jquery element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetfunction = (tweet) => {
+    let $tweet = 
+        `<article>
+            <header>
+                <img src="${tweet.avatars}" />
+                <p>
+                    <span>${tweet.name}<span>
+                    <span>${tweet.handle}<span>
+                </p>
+            </header>
+            <div class="tweet-content">
+                ${tweet.content.text}
+            </div>
+            <footer></footer>
+        </article`;
+
+    return $tweet;
+}
