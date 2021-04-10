28b99eecddba3e1bade85c23f883f9cc106f2c10
Catherine Li <catherine.l@intiveo.com>
2021-04-09 19:16:35 -0700

    Added some styling to the new tweets container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,15 +3,39 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-const createTweetfunction = (tweet) => {
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1617842642225
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1617929042225
+    }
+]
+const createTweet = (tweet) => {
+    
     let $tweet = 
         `<article>
             <header>
-                <img src="${tweet.avatars}" />
+                <img src="${tweet.user.avatars}" />
                 <p>
-                    <span>${tweet.name}<span>
-                    <span>${tweet.handle}<span>
+                    <span>${tweet.user.name}</span>
+                    <span>${tweet.user.handle}</span>
                 </p>
             </header>
             <div class="tweet-content">
@@ -22,3 +46,20 @@ const createTweetfunction = (tweet) => {
 
     return $tweet;
 }
+
+const renderTweets = (tweets) => {
+    // for(tweet in tweets) {
+    //     console.log(tweet);
+    //     $("#tweets-container").append(createTweet(tweet));
+    // }
+
+    tweets.forEach(tweet => {
+
+        let $newTweet = createTweet(tweet);
+        console.log($newTweet)
+        $("#tweets-container").append($newTweet);
+    });
+}
+$(document).ready(function(){
+    renderTweets(data);
+})
